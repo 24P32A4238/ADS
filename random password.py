9f7ee2793519ddae4942dd5825543fa52261d325
24P32A4238 <vanaparthibhaskar616@gmail.com>
2025-12-30 19:05:51 +0530

    Add files via upload

---
+++ random password.py
@@ -0,0 +1,10 @@
+
+import random
+import string
+
+length = int(input("Enter password length: "))
+
+chars = string.ascii_letters + string.digits + "!@#$%^&*"
+password = "".join(random.choice(chars) for _ in range(length))
+
+print("Generated Password:", password)
